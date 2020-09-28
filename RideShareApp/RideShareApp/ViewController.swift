12704ee95a2b934f15f81c87a49d484e6c8d0907
farazhaider88 <farazhaider88@yahoo.com>
2020-09-28 22:34:07 +0500

    pod and assets
    pods and assets added.

---
+++ RideShareApp/RideShareApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RideShareApp
+//
+//  Created by Faraz Haider on 27/09/2020.
+//  Copyright © 2020 Faraz Haider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
